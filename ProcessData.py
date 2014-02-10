019c085ed7d24c9ba9566ee67a3ce226c4905b0e
Shayan Doroudi <ShayanDoroudi@Shayans-MacBook-Air.local>
2014-02-10 16:55:03 -0500

    Modified how the log file is processed, such that observations are 0/1 for each step, and actions are the problem number for each step.

--- ProcessData.py
+++ ProcessData.py
@@ -1,8 +1,9 @@
 import sys
 
+sys.path.append('/Users/ShayanDoroudi/Dropbox/Research/svnrepo/code')
 from hmmEM import em
 
-filename = '../Data/ManchesterDataset.txt'
+filename = '../../../Data/ManchesterDataset.txt'
 problemsets = ['Manchester Fractions Study 2013 Comparison']
 
 
@@ -20,9 +21,9 @@ while row != ['']:
 	if row[3] != curr_student and row[13] in problemsets:
 		curr_student = row[3]
 		curr_prob = None
-		if len(obs) > 0 and [] in obs[-1]:
-			actions[-1].pop(-1)
-			obs[-1].pop(-1)
+		if obs[-1] == []:
+			actions.pop(-1)
+			obs.pop(-1)
 		actions.append([])
 		obs.append([])
 		# i += 1
@@ -33,21 +34,28 @@ while row != ['']:
 	if row[14] != curr_prob:
 		curr_prob = row[14]
 		# a += 1
-		if not (len(obs[-1]) > 0 and obs[-1][-1] == []):
-			obs[-1].append([])
-		else:
-			actions[-1].pop(-1)
+		# if not (len(obs[-1]) > 0 and obs[-1][-1] == []):
+			# obs[-1].append([])
+		# else:
+		# 	actions[-1].pop(-1)
 		actions[-1].append(int(curr_prob))
 		fakeSteps = fakeSteps[0:3]
 	if row[17] not in fakeSteps:
-		obs[-1][-1].append(1 if row[19] == 'Correct' else 0)
+		obs[-1].append(1 if row[19] == 'Correct' else 0)
+		actions[-1].append(int(curr_prob))
+		fakeSteps.append(row[17])
+	# if row[17] not in fakeSteps:
+	# 	obs[-1][-1].append(1 if row[19] == 'Correct' else 0)
+	# 	fakeSteps.append(row[17])
 		# fakeSteps[-1] = row[16] # In case the previous step was InCorrect, we don't want to double count it.
 	# elif row[16] != fakeSteps[-1]:
 	# 	fakeSteps[-1] = None
-		fakeSteps.append(row[17])
 	row = file.readline().split('\t')
 
 file.close()
+
+print len(obs)
+print len(actions)
 
 # Can be used when observations are "ternary" -- 0 for incorrect, 1 for correct, 2 for hint
 
@@ -68,11 +76,11 @@ file.close()
 
 # Can be used to convert observation from 0-1 vector to a decimal number
 
-def bin2dec(terlst):
-	tot = 0
-	for (i, num) in enumerate(terlst):
-		tot += num * 2**(len(terlst) - i - 1)
-	return tot
+# def bin2dec(terlst):
+# 	tot = 0
+# 	for (i, num) in enumerate(terlst):
+# 		tot += num * 2**(len(terlst) - i - 1)
+# 	return tot
 
 # decobs = []
 # for s in obs:
@@ -88,9 +96,9 @@ for i in range(len(actions)):
 		opa[actions[i][j]] = 2
 
 # newobs will be 0 if less than half the steps were correct and 1 otherwise.		
-newobs = []
-for o in obs:
-	newobs.append(map(lambda e: int(e.count(1) > len(e)/2.), o))
-print newobs
+# newobs = []
+# for o in obs:
+# 	newobs.append(map(lambda e: int(e.count(1) > len(e)/2.), o))
+# print newobs
 
-print em(newobs[1:], actions[1:], 4, 36, opa, 5)
+print em(obs, actions, 4, 36, opa, 5)
